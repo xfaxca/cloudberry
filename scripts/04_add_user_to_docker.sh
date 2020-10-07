684a2fa86b05c0a551065482d9733d03137b03b8
xfaxca <Cameron@tutanota.com>
2020-10-07 18:18:28 -0500

    Adjusted docker script

--- scripts/04_add_user_to_docker.sh
+++ scripts/04_add_user_to_docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+usr=$1
 
 # 1. Add docker group
 sudo groupadd docker
 
 # 2. Add current user to docker group
-sudo gpasswd -a $USER docker
+
+sudo gpasswd -a $usr docker
 
 # 3. Test sudo-less run
 docker run hello-world
