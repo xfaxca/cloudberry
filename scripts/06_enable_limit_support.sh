0b3f24cbdd865de5c3e221b17a82796133f2eb5d
xfaxca <Cameron@tutanota.com>
2020-10-07 19:02:37 -0500

    script updates

--- scripts/06_enable_limit_support.sh
+++ scripts/06_enable_limit_support.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# For raspberry pi, need to these to /boot/firmware/cmdline.txt. Suggested by warnings from `docker info`
+# For raspberry pi, need to these to /boot/firmware/cmdline.txt. Suggested by warnings from `docker info`.
+# Enabling these settings should remove those error warnings.
 # cgroup_enable=cpuset
 # cgroup_enable=memory
 # cgroup_memory=1
--- scripts/07_allow_iptable_bridged_traffic.sh
+++ scripts/07_allow_iptable_bridged_traffic.sh
@@ -1,6 +1,7 @@
 # According to the documentation, Kubernetes needs iptables to be configured to see bridged network traffic to ensure that 
 # Kubernetes cluster addresses go via the appropriate adapter if Kube components are not reachable on the default route. 
-# The following script does this:
+# Doc link: https://v1-18.docs.kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#letting-iptables-see-bridged-traffic
+# The following command does this:
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
