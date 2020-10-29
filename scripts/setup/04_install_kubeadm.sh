df7a50c6c45d90d338136b406b3d50dddf149973
xfaxca <Cameron@tutanota.com>
2020-10-29 18:53:12 -0500

    Removed /boot/cmdline.txt mods. Taken care of after docker install and newest location is /boot/firmware/cmdline.txt

--- scripts/setup/04_install_kubeadm.sh
+++ scripts/setup/04_install_kubeadm.sh
@@ -9,10 +9,6 @@ fi
 sudo dphys-swapfile swapoff && \
 sudo dphys-swapfile uninstall && \
 sudo update-rc.d dphys-swapfile remove
-echo Adding " cgroup_enable=cpuset cgroup_enable=memory" to /boot/cmdline.txt
-sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
-orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_enable=memory"
-echo $orig | sudo tee /boot/cmdline.txt
 
 # Add repo list and install kubeadm
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
